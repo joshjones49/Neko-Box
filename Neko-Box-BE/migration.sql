0ab4caaf9ef8be9a03abe45d825d5816e61e2c7e
joshjones49 <joshjones200285@gmail.com>
2025-03-14 21:04:42 -0500

    filled database with shows table

--- Neko-Box-BE/migration.sql
+++ Neko-Box-BE/migration.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS shows
+DROP TABLE IF EXISTS shows;
 
 CREATE TABLE shows (
     id SERIAL PRIMARY KEY,
@@ -7,4 +7,16 @@ CREATE TABLE shows (
     description TEXT,
     viewer_rating VARCHAR(255),
     quality_rating INTEGER
-)
+);
+
+INSERT INTO shows (name, category, description, viewer_rating, quality_rating) VALUES
+
+('Berserk', 'Seinen', 'DESCRIPTION', 'Mature', 5),
+
+('Attack On Titan', 'Shonen', 'DESCRIPTION', 'Teen', 5),
+
+('Sailor Moon', 'Shoujo', 'DESCRIPTION', 'Kids', 5),
+
+('Bunny Drop', 'Josei', 'DESCRIPTION', 'Teen', 3 ),
+
+('Ms. Koboyashi: Dragon Maid', 'Slice of Life', 'DESCRIPTION', 'Teen', 4);
