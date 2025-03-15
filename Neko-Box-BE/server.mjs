543823f8dcbcbdf3bf2342b3126523b171f00b0b
joshjones49 <joshjones200285@gmail.com>
2025-03-14 22:00:26 -0500

    created get all route

--- Neko-Box-BE/server.mjs
+++ Neko-Box-BE/server.mjs
@@ -6,13 +6,13 @@ import pg from 'pg';
 
 // CONFIGURATION
 dotenv.config();
-const PORT = process.env.PORT || 8008;
+const PORT = 8008;
 const { Pool } = pg;
 const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
 });
 
@@ -23,9 +23,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ROUTES
 
-
-
+// get all
+app.get('/', async (req, res) => {
+    try {
+      const result = await pool.query("SELECT * FROM shows");
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: error.message});
+    }
+});
 
 
 
