a95feaa2e34552592f9952421bf2c58662974d41
joshjones49 <joshjones200285@gmail.com>
2025-03-14 23:14:02 -0500

    improving navbar css

--- Neko-Box/src/components/Navbar/Navbar.jsx
+++ Neko-Box/src/components/Navbar/Navbar.jsx
@@ -5,8 +5,17 @@ import { SiGnuicecat } from "react-icons/si";
 const Navbar = () => {
   return (
     <div id="navbar" >
-      <div>
-      <SiGnuicecat className='neko'/>
+      <div className='nav-divs left-div' >
+        <div>
+          <SiGnuicecat className='neko'/>
+          <h1>Neko Box</h1>
+        </div>
+        <div><h3>New</h3></div>
+        <div><h3>Popular</h3></div>
+      </div>
+      <div className='nav-divs right-div' >
+        <input placeholder='search' />
+        <div><h3>Watchlist</h3></div>
       </div>
     </div>
   )
