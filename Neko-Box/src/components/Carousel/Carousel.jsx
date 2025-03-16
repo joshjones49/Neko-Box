25db5b0d07b77d7ffd06bc65fd3c123a82ae54aa
joshjones49 <joshjones200285@gmail.com>
2025-03-16 10:57:21 -0500

    basic styling for carousel finished

--- Neko-Box/src/components/Carousel/Carousel.jsx
+++ Neko-Box/src/components/Carousel/Carousel.jsx
@@ -1,10 +1,17 @@
 import './Carousel.css'
 import React from 'react'
+import { SiGnuicecat } from "react-icons/si"
 
 const Carousel = () => {
   return (
     <div className='carousel' >
-      CAROUSEL
+      <div>
+        <div>
+        <SiGnuicecat className='neko'/>
+        <h1>NEKO BOX</h1>
+        </div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae ad voluptate natus tempora corporis, in libero rem fuga dicta facilis illo incidunt neque velit itaque excepturi praesentium asperiores laborum. Aliquid.</p>
+      </div>
     </div>
   )
 }
